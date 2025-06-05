f6a00ea9e418d74fcece82e687bdd9a201a4a2d0
JulietaG13 <julieta.garberoglio@gmail.com>
2025-06-04 22:07:05 -0300

    Clean tests

--- test/specs/welcome.e2e.ts
+++ test/specs/welcome.e2e.ts
@@ -14,34 +14,17 @@ describe('Welcome Screen', () => {
         await welcomeScreen.verifyWelcomeScreenElements();
     });
 
-    it('should have clickable buttons', async () => {
-        // Verify buttons are clickable
-        await welcomeScreen.verifyButtonsAreClickable();
-    });
-
-    it('should display correct button texts', async () => {
-        // Verify button texts
-        await welcomeScreen.verifyButtonTexts();
-    });
-
     it('should navigate to registration when Get Started is tapped', async () => {
-        // Tap Get Started button
+        await welcomeScreen.waitForWelcomeScreenLoaded();
         await welcomeScreen.tapGetStarted();
-        
-        // Here you would verify navigation to registration screen
-        // For now, we'll just take a screenshot
+
         await browser.pause(2000); // Wait for navigation
     });
 
     it('should navigate to login when Sign In is tapped', async () => {
-        // First ensure we're on welcome screen
         await welcomeScreen.waitForWelcomeScreenLoaded();
-        
-        // Tap Sign In button
         await welcomeScreen.tapSignIn();
-        
-        // Here you would verify navigation to login screen
-        // For now, we'll just take a screenshot
+
         await browser.pause(2000); // Wait for navigation
     });
 }); 
